6abd9ff888859c7cb28f50b4986d1717e4de2a12
Daniel Christian Kup <danielc.kup@gmail.com>
2023-10-30 22:37:53 +0100

    rename parser.rs to logfile_parser.rs

--- src/processing.rs
+++ src/processing.rs
@@ -1,7 +1,7 @@
 #![warn(clippy::all)]
 
 pub mod app_state;
-pub mod parser;
+pub mod logfile_parser;
 pub mod project_type;
 pub mod task;
 pub mod task_state;
@@ -9,7 +9,7 @@ pub mod timestamps;
 pub mod zeit;
 
 pub use app_state::*;
-pub use parser::*;
+pub use logfile_parser::*;
 pub use project_type::*;
 pub use task::*;
 pub use task_state::*;
